371494989899b8e1485cf839a3a3cf6c8a04ad1f
Jeremy <jclifton44@gmail.com>
2016-03-12 13:36:38 -0600

    load images - *not tested*

--- app/src/main/java/com/example/jesusify/Gallery.java
+++ app/src/main/java/com/example/jesusify/Gallery.java
@@ -1,5 +1,7 @@
 package com.example.jesusify;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.ActionBar;
@@ -11,18 +13,45 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnDragListener;
 
+import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.jesusify.Secondary_Activity;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Gallery extends Activity {
+    private Integer imageCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
         HorizontalScrollView hsv = (HorizontalScrollView) findViewById(R.id.galleryPhotoView);
         int scrollViewWidth = hsv.getWidth();
+        loadImages();
+        if(imageCount == 0 ) {
+
+        } else if(imageCount == 1) {
+            int imageDistance1 = 0;
+            int imageDistance2 = scrollViewWidth;
+            int imageDistance3 = scrollViewWidth;
+
+        } else if (imageCount == 2) {
+            int imageDistance1 = 0;
+            int imageDistance2 = scrollViewWidth/2;
+            int imageDistance3 = scrollViewWidth;
+
+        } else if (imageCount >= 3) {
+            int imageDistance1 = 0;
+            int imageDistance2 = scrollViewWidth/3;
+            int imageDistance3 = 2*scrollViewWidth/3;
+        }
+
+
+
         //image1  0
         //image2 w/3
         //image3 2*w/3
@@ -39,7 +68,50 @@ public class Gallery extends Activity {
 
     }
 
+    public void loadImages() {
+        File container = new File(Secondary_Activity.storagePath);
+        ArrayList<String> galleryList = new ArrayList<String>();
+        for(String s: container.list()){
+            if(s.contains("zeusify_")) {
+                galleryList.add(s);
+            }
+        }
+        ImageView image1 = (ImageView) findViewById(R.id.image1);
+        ImageView image2 = (ImageView) findViewById(R.id.image2);
+        ImageView image3 = (ImageView) findViewById(R.id.image3);
+        Bitmap image1BM = null;
+        Bitmap image2BM = null;
+        Bitmap image3BM = null;
+        imageCount = galleryList.size();
+        if(galleryList.size() == 0) {
+            //set display to 'no photos'
 
+        }
+        if(galleryList.size() == 1) {
+            image1BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + galleryList.get(0));
+            image1.setImageBitmap(image1BM);
+            ((ViewGroup)image1.getParent()).removeView(image2);
+            ((ViewGroup)image1.getParent()).removeView(image3);
+            //take two images off
+        }
+        if(galleryList.size() == 2) {
+            image1BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + galleryList.get(0));
+            image2BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + galleryList.get(1));
+            image1.setImageBitmap(image1BM);
+            image2.setImageBitmap(image2BM);
+            ((ViewGroup)image1.getParent()).removeView(image3);
+
+        }
+        if (galleryList.size() >= 3) {
+            image1BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + galleryList.get(0));
+            image2BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + galleryList.get(1));
+            image3BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + galleryList.get(2));
+            image1.setImageBitmap(image1BM);
+            image2.setImageBitmap(image2BM);
+            image3.setImageBitmap(image3BM);
+
+        }
+    }
 
     @Override
     public void onDestroy()
