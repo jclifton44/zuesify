6c067a65d12c8e007fcb5754b17ea5ab9acb2492
Jeremy <jclifton44@gmail.com>
2016-03-15 22:31:54 -0500

    all photos viewable from gallery

--- app/src/main/java/com/example/jesusify/GalleryHorizontalScrollView.java
+++ app/src/main/java/com/example/jesusify/GalleryHorizontalScrollView.java
@@ -9,6 +9,7 @@ import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -29,6 +30,7 @@ public class GalleryHorizontalScrollView extends HorizontalScrollView {
     public static ImageView image2;
     public static ImageView image3;
     public static ArrayList<String> galleryList = new ArrayList<String>();
+    boolean InDrag = false;
     public GalleryHorizontalScrollView(Context context) {
         super(context);
     }
@@ -54,40 +56,12 @@ public class GalleryHorizontalScrollView extends HorizontalScrollView {
             }
         }
         Log.d(GalleryHorizontalScrollView.galleryList.size() + "", "Size");
-        GalleryHorizontalScrollView.image1 = (ImageView) findViewById(R.id.image1);
-        GalleryHorizontalScrollView.image2 = (ImageView) findViewById(R.id.image2);
-        GalleryHorizontalScrollView.image3 = (ImageView) findViewById(R.id.image3);
-        Bitmap image1BM = null;
-        Bitmap image2BM = null;
-        Bitmap image3BM = null;
-        imageCount = galleryList.size();
-        if(galleryList.size() == 0) {
-            //set display to 'no photos'
-
-        }
-        if(galleryList.size() == 1) {
-            image1BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + "/DCIM/Camera/" + galleryList.get(0));
-            GalleryHorizontalScrollView.image1.setImageBitmap(image1BM);
-            ((ViewGroup)GalleryHorizontalScrollView.image1.getParent()).removeView(image2);
-            ((ViewGroup)GalleryHorizontalScrollView.image1.getParent()).removeView(image3);
-            //take two images off
-        }
-        if(galleryList.size() == 2) {
-            image1BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + "/DCIM/Camera/" + galleryList.get(0));
-            image2BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + "/DCIM/Camera/" + galleryList.get(1));
-            GalleryHorizontalScrollView.image1.setImageBitmap(image1BM);
-            GalleryHorizontalScrollView.image2.setImageBitmap(image2BM);
-            ((ViewGroup)GalleryHorizontalScrollView.image1.getParent()).removeView(image3);
-
-        }
-        if (galleryList.size() >= 3) {
-            image1BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + "/DCIM/Camera/" + galleryList.get(0));
-            image2BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + "/DCIM/Camera/" + galleryList.get(1));
-            image3BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + "/DCIM/Camera/" + galleryList.get(2));
-            GalleryHorizontalScrollView.image1.setImageBitmap(image1BM);
-            GalleryHorizontalScrollView.image2.setImageBitmap(image2BM);
-            GalleryHorizontalScrollView.image3.setImageBitmap(image3BM);
-
+        LinearLayout photoFrame = (LinearLayout) findViewById(R.id.photoFrame);
+        for( int i = 0; i < galleryList.size(); i++) {
+            ImageView imageView = new ImageView(getContext());
+            Bitmap image = BitmapFactory.decodeFile(Secondary_Activity.storagePath + "/DCIM/Camera/" + galleryList.get(i));
+            photoFrame.addView(imageView);
+            imageView.setImageBitmap(image);
         }
     }
     @Override
@@ -97,6 +71,7 @@ public class GalleryHorizontalScrollView extends HorizontalScrollView {
         Log.d(ev.getAction() + "", " ACTION");
         switch(ev.getAction()) {
             case MotionEvent.ACTION_DOWN: {
+                InDrag = true;
                 Log.d("down", "down");
                 if(getChildCount() == 0) {
                     return false;
@@ -106,10 +81,13 @@ public class GalleryHorizontalScrollView extends HorizontalScrollView {
             }
             case MotionEvent.ACTION_MOVE: {
                 Log.d("MOTION_", ev.getAction() + "MOVE");
-                scrollTo((ImageWidth * ImageIndex) + Math.round(startLocation - ev.getX()), 0);
+                if((ImageWidth * ImageListIndex) + Math.round(startLocation - ev.getX()) <= ImageWidth * ImageListIndex){
+                    scrollTo((ImageWidth * ImageListIndex) + Math.round(startLocation - ev.getX()), 0);
+                }
                 break;
             }
             case MotionEvent.ACTION_UP: {
+                InDrag = false;
                 Log.d("ACTION", ev.getAction() + " UP");
                 Log.d("" + Math.abs(startLocation - ev.getX()), ImageWidth + "");
                 if(Math.abs(startLocation - ev.getX()) > ImageWidth / 2) {
@@ -119,38 +97,23 @@ public class GalleryHorizontalScrollView extends HorizontalScrollView {
                         if (ImageListIndex != 0) {
                             ImageListIndex--;
                         }
-                        if (ImageIndex != 0) {
-                            ImageIndex--;
-                        }
-                        smoothScrollTo(ImageIndex*ImageWidth,0);
-
-                        //animate to left
                     } else {
                         Log.d("greater than", "less");
-                        if (ImageListIndex != imageCount) {
+                        if (ImageListIndex != galleryList.size() - 1) {
                             ImageListIndex++;
                         }
-                        smoothScrollTo(ImageIndex*ImageWidth,0);
-                        //animate to right
-                        if (ImageIndex != 2) {
-                            ImageIndex++;
-                        }
-                        if (ImageIndex == 2) {
-                            ImageIndex = 1;
-                            image1.setImageBitmap(image2BM);
-                            image1BM = image2BM;
-                            image2.setImageBitmap(image3BM);
-                            image2BM = image3BM;
-                            image3BM = BitmapFactory.decodeFile(Secondary_Activity.storagePath + "/DCIM/Camera/" + galleryList.get(ImageListIndex));
-                            //scrollTo(ImageIndex * ImageWidth, 0);
-                            image3.setImageBitmap(image3BM);
-                        }
+
                     }
+
                 }
+                smoothScrollTo(ImageListIndex*ImageWidth,0);
+
 
                 break;
             }
         }
         return true;
     }
+
+
 }
